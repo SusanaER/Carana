9490f38b785c324acd1d2f39aee6e2e5c646dfa1
Alberto Campos <alberto.campos@heyirys.com>
2021-12-07 13:36:56 -0600

    Fixed breaking changes

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Home from "./screens/Home";
 import ToDoList from "./screens/ToDoList";
 import EditList from "./screens/EditList";
@@ -20,8 +20,8 @@ import arracadas from './elements/arracadas';
 import arracadasTriples from './elements/arracadasTriples';
 import huggiess from './elements/Huggiess';
 
-const Stack = createStackNavigator();
-const AuthStack = createStackNavigator();
+const Stack = createNativeStackNavigator();
+const AuthStack = createNativeStackNavigator();
 
 const AuthScreens = () => {
     return (
